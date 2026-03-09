8498a06d40070341de6fdb8a40db84076384534c
eddy2809 <edoardotantari99@gmail.com>
2026-03-09 11:52:46 +0100

    add wand support and refactoring

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,5 +1,7 @@
 import json
 import torch
+import wandb
+import os
 import evaluate
 import numpy as np
 from datasets import Dataset
@@ -13,9 +15,9 @@ from transformers import (
 )
 
 # --- 1. CONFIGURAZIONE ESPERIMENTO ---
-FILE_TRAIN = "json_datasets/multilanguage/dataset_train_full.json" 
-FILE_TEST = "json_datasets/multilanguage/dataset_test.json"
-NOME_MODELLO_SALVATO = "model/mul_roberta_medico_full_shot_early"
+FILE_TRAIN = "data/processed/multi/dataset_train_full.json" 
+FILE_TEST = "data/processed/multi/dataset_test.json"
+NOME_MODELLO_SALVATO = "multi_roberta_medico_full_shot_early"
 
 #MODEL_NAME = "dbmdz/bert-base-italian-cased"
 #MODEL_NAME = "bert-base-multilingual-cased"
@@ -30,14 +32,15 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 seqeval = evaluate.load("seqeval") # Metrica standard per il NER
 
 # --- 2. FUNZIONI DI SUPPORTO (Riutilizziamo la tua funzione vincente) ---
-def allinea_etichette_bio(testo, entita_estratte, tokenizer):
+def BIO_encoding(testo, entita_estratte, tokenizer):
     
     """ Prende il testo grezzo e le coordinate, e restituisce i token di BERT 
     con le relative etichette B-I-O. """
 
     tokenized = tokenizer(testo, return_offsets_mapping=True, truncation=True, max_length=512)
     offsets = tokenized["offset_mapping"]
-    
+    # per avere i token come stringa (DEBUG)
+    input_ids = tokenized["input_ids"]
     # Inizializza tutto a 'O' (0)
     labels = [label2id['O']] * len(offsets)
     
@@ -54,45 +57,37 @@ def allinea_etichette_bio(testo, entita_estratte, tokenizer):
                     labels[idx] = label2id['B-CLINENTITY']
                 else:
                     labels[idx] = label2id['I-CLINENTITY']
+
+    # Convertiamo gli ID in token testuali
+    tokens_text = tokenizer.convert_ids_to_tokens(input_ids)
+    BIO_list = []
+    for t, l in zip(tokens_text, labels):
+        label_name = id2label[l] if l != -100 else "SPECIAL"
+        BIO_list.append((t, label_name))
                     
     # Non ci servono più gli offsets per il training
     tokenized["labels"] = labels
     del tokenized["offset_mapping"] 
-    return tokenized
+    return tokenized, BIO_list
 
-def prepara_dataset(file_json):
-    """Legge il file JSON salvato prima e lo trasforma in un Dataset HuggingFace."""
+def make_dataset(file_json):
+    """Legge il file JSON e lo trasforma in un Dataset HuggingFace."""
     with open(file_json, 'r', encoding='utf-8') as f:
         dati = json.load(f)
         
     all_inputs = {"input_ids": [], "attention_mask": [], "labels": []}
     
     for doc in dati:
-        # Applichiamo l'allineamento a ogni documento
-        tok_doc = allinea_etichette_bio(doc["text"], doc["entities"], tokenizer)
-        all_inputs["input_ids"].append(tok_doc["input_ids"])
-        all_inputs["attention_mask"].append(tok_doc["attention_mask"])
-        all_inputs["labels"].append(tok_doc["labels"])
+        if doc is not None:
+            # Applichiamo l'allineamento a ogni documento
+            tok_doc, token_text = BIO_encoding(doc["text"], doc["entities"], tokenizer)
+            all_inputs["input_ids"].append(tok_doc["input_ids"])
+            all_inputs["attention_mask"].append(tok_doc["attention_mask"])
+            all_inputs["labels"].append(tok_doc["labels"])
         
-    return Dataset.from_dict(all_inputs)
+    return Dataset.from_dict(all_inputs), token_text
 
-# --- 3. PREPARAZIONE DATI E MODELLO ---
-print(f"Preparazione dei dati per l'esperimento: {FILE_TRAIN}...")
-train_dataset = prepara_dataset(FILE_TRAIN)
-eval_dataset = prepara_dataset(FILE_TEST)
-
-print("Caricamento del modello BERT...")
-modello = AutoModelForTokenClassification.from_pretrained(
-    MODEL_NAME, 
-    num_labels=len(label_list),
-    id2label=id2label,
-    label2id=label2id
-)
-
-# Il DataCollator si assicura che tutte le frasi abbiano la stessa lunghezza riempiendole di zeri
-data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
-
-# --- 4. METRICHE DI VALUTAZIONE ---
+# --- 3. METRICHE DI VALUTAZIONE ---
 def compute_metrics(p):
     """Calcola Precision, Recall e F1-Score (Quelli che metterai nel grafico per il prof!)"""
     predictions, labels = p
@@ -116,44 +111,78 @@ def compute_metrics(p):
         "accuracy": results["overall_accuracy"],
     }
 
-# --- 5. IL MOTORE DEL TRAINING DI HUGGINGFACE ---
-training_args = TrainingArguments(
-    output_dir=f"./risultati_{NOME_MODELLO_SALVATO}",
-    eval_strategy="epoch",
-    save_strategy="epoch",
-    learning_rate=2e-5,
-    per_device_train_batch_size=8, 
-    per_device_eval_batch_size=8, #prima era 16 per full shot
-    num_train_epochs=50, # 5 epoche sono perfette per il Few-Shot
-    weight_decay=0.01,
-    bf16=True, 
-    save_only_model=True,   # Evita di salvare optimizer/scheduler (molto pesanti)
-    metric_for_best_model="f1",
-    load_best_model_at_end=True,
-    greater_is_better=True,
-    save_total_limit=1,
-    
-    # warmup_ratio=0.1,             # Il 10% dei passi iniziali serve a "scaldare" il modello , strategia warmup
-    # lr_scheduler_type="cosine",   # La curva di discesa morbida
+wandb.init(
+    project="clinical-ner-task", 
+    name="multi_fullFT__early_roberta",
+    tags=["BERT", "NER", "Clinical"]
 )
 
-trainer = Trainer(
-    model=modello,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=eval_dataset,
-    processing_class=tokenizer,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics,
-    callbacks=[EarlyStoppingCallback(early_stopping_patience=5)]
-)
+if __name__ == "__main__":
 
-# VIA AL TRAINING!
-print("\n🚀 INIZIO ADDESTRAMENTO 🚀")
-trainer.train()
+    from dotenv import load_dotenv
 
-# Salviamo il modello finetunato
-trainer.save_model(NOME_MODELLO_SALVATO)
-print(f"Modello salvato in: {NOME_MODELLO_SALVATO}")
+    load_dotenv()
+    api_key = os.getenv("WANDB_API_KEY")
+    wandb.login(key=api_key)
 
-trainer.state.save_to_json(f"./model{NOME_MODELLO_SALVATO}/trainer_state.json")
+
+    # --- 4. PREPARAZIONE DATI E MODELLO ---
+    print(f"Preparazione dei dati per l'esperimento: {FILE_TRAIN}...")
+    train_dataset,_ = make_dataset(FILE_TRAIN)
+    eval_dataset,_ = make_dataset(FILE_TEST)
+
+    print("Caricamento del modello BERT...")
+    modello = AutoModelForTokenClassification.from_pretrained(
+        MODEL_NAME, 
+        num_labels=len(label_list),
+        id2label=id2label,
+        label2id=label2id
+    )
+
+    # Il DataCollator si assicura che tutte le frasi abbiano la stessa lunghezza riempiendole di zeri
+    data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+
+
+
+    # --- 5. IL MOTORE DEL TRAINING DI HUGGINGFACE ---
+    training_args = TrainingArguments(
+        output_dir=f"./risultati_model/{NOME_MODELLO_SALVATO}",
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=8, 
+        per_device_eval_batch_size=8, #prima era 16 per full shot
+        num_train_epochs=50, # 5 epoche sono perfette per il Few-Shot
+        weight_decay=0.01,
+        bf16=True, 
+        save_only_model=True,   # Evita di salvare optimizer/scheduler (molto pesanti)
+        metric_for_best_model="f1",
+        load_best_model_at_end=True,
+        greater_is_better=True,
+        save_total_limit=1,
+        report_to="wandb"
+        
+        # warmup_ratio=0.1,             # Il 10% dei passi iniziali serve a "scaldare" il modello , strategia warmup
+        # lr_scheduler_type="cosine",   # La curva di discesa morbida
+    )
+
+    trainer = Trainer(
+        model=modello,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        processing_class=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=5)]
+    )
+
+    # VIA AL TRAINING!
+    print("\n🚀 INIZIO ADDESTRAMENTO 🚀")
+    trainer.train()
+
+    # Salviamo il modello finetunato
+    trainer.save_model(f"model/{NOME_MODELLO_SALVATO}")
+    print(f"Modello salvato in: model/{NOME_MODELLO_SALVATO}")
+
+    trainer.state.save_to_json(f"model/{NOME_MODELLO_SALVATO}/trainer_state.json")
